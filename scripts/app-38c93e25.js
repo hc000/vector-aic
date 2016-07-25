c7ff425d4930f687f165c7c820e28bdf7c16b0e3
hc000 <hc000@users.noreply.github.com>
2016-07-25 10:16:25 -0700

    test

--- scripts/app-38c93e25.js
+++ scripts/app-38c93e25.js
@@ -1,24 +1,5 @@
-var versionIE = detectIE();
-if (versionIE === false) {
+if (window.navigator.userAgent.indexOf('MSIE ') !==-1) {
 	alert("You are using an unsupported browser, Internet Explorer (IE). Please use FireFox or Chrome.");
-}
-document.getElementById('details').innerHTML = window.navigator.userAgent;
-function detectIE() {
-  var ua = window.navigator.userAgent;
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-  }
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-  }
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-  }
-  return false;
 }
 /**!
  *
